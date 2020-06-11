154a57f569586b16ed6b45a0a345791fabf19cef
mohsenie <msh440@gmail.com>
2020-06-12 01:02:07 +0200

    fixing the target logger class

--- client/src/main/java/uk/co/blackpepper/bowman/CacheCommands.java
+++ client/src/main/java/uk/co/blackpepper/bowman/CacheCommands.java
@@ -11,7 +11,7 @@ import java.sql.*;
 
 public class CacheCommands implements CacheCommandsInterface{
 
-    private final Logger logger = LoggerFactory.getLogger(DefaultCacheManager.class);
+    private final Logger logger = LoggerFactory.getLogger(CacheCommands.class);
     private final Connection conn;
     private final String tableName;
 
